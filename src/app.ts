946ce316fa2cb1d89b5b6ce439c9ccd6243d5180
Ndekocode <arickbulakali@gmail.com>
2024-08-30 08:20:52 +0200

    add fromTo to solve from bug error

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { gsap } from 'gsap';
 
 window.addEventListener("DOMContentLoaded", () => {
-  const img1 = document.querySelector(".container-images img:nth-child(1)");
+  const img1 = document.querySelector(".container-images img:nth-child(1)") as HTMLImageElement;
+  const img2 = document.querySelector(".container-images img:nth-child(2)")  as HTMLImageElement;
   const btn = document.querySelector(".btn");
+
   btn?.addEventListener("click", () => {
     gsap.fromTo(
       img1,
@@ -13,10 +15,13 @@ window.addEventListener("DOMContentLoaded", () => {
       {
         x: 100,
         y: 100,
-        opacity:1,
+        opacity: 1,
         scale: 1.5,
-        duration: 2,
       }
     );
+    gsap.to(img2, {
+      y: 100,
+      delay: 2,
+    });
   });
 });
