6ebc15c7b3796e330aafe42d605efb8b18e8f5af
Ndekocode <arickbulakali@gmail.com>
2024-08-30 10:12:08 +0200

    add easing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import { gsap } from 'gsap';
 
 window.addEventListener("DOMContentLoaded", () => {
-  const img1 = document.querySelector(".container-images img:nth-child(1)") as HTMLImageElement;
-  const img2 = document.querySelector(".container-images img:nth-child(2)")  as HTMLImageElement;
+  const img1 = document.querySelector(
+    ".container-images img:nth-child(1)"
+  ) as HTMLImageElement;
+  const img2 = document.querySelector(
+    ".container-images img:nth-child(2)"
+  ) as HTMLImageElement;
+  const img3 = document.querySelector(
+    ".container-images img:nth-child(3)"
+  ) as HTMLImageElement;
   const btn = document.querySelector(".btn");
 
+  gsap.to([img1, img2, img3], {
+     scale: 1.5,
+    y: 300,
+    duration:3.5,
+    delay: 2,
+    ease:"circ"
+  });
   btn?.addEventListener("click", () => {
     gsap.fromTo(
       img1,
@@ -22,6 +36,7 @@ window.addEventListener("DOMContentLoaded", () => {
     gsap.to(img2, {
       y: 100,
       delay: 2,
+      repeat: 2,
     });
   });
 });
