8b5e13114e28ca295303ecc3e8d61d55b1829d3d
Ndekocode <arickbulakali@gmail.com>
2024-09-01 12:35:41 +0200

    refactor: add random function to x position

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,19 @@ window.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector(".btn");
 
   gsap.to([img1, img2, img3], {
-     scale: 1.5,
+    scale: 1.5,
     y: 300,
-    duration:3.5,
+    duration: 3.5,
     delay: 2,
-    stagger:{
-      amount:0.35,
+    stagger: {
+      amount: 0.35,
     },
-    ease:"circ"
+    ease: "circ",
+  });
+  gsap.to(".btn", {
+    x: "random(100,-100)",
+    duration: 0.35,
+    stagger: 0.2,
   });
   btn?.addEventListener("click", () => {
     gsap.fromTo(
